863636c86514540ddda523cc1cab4852ed208ed5
oboe-wan-kenobi <62673737+oboe-wan-kenobi@users.noreply.github.com>
2020-07-23 08:28:26 -0500

    MIDIReciever Updates

--- IPlugExamples/SynthesizerTwo/MIDIReceiver.cpp
+++ IPlugExamples/SynthesizerTwo/MIDIReceiver.cpp
@@ -1,1 +1,10 @@
 #include "MIDIReceiver.h"
+//pretzel
+
+void MIDIReceiver::onMessageReceived(IMidiMsg* midiMessage) {
+    IMidiMsg::EStatusMsg status = midiMessage->StatusMsg();
+    // We're only interested in Note On/Off messages (not CC, pitch, etc.)
+    if (status == IMidiMsg::kNoteOn || status == IMidiMsg::kNoteOff) {
+        mMidiQueue.Add(midiMessage);
+    }
+}
